d12397ea09d17214198ebbec41700b4beb55d8f5
Steffen Schröder <steffen@schroeder-blog.de>
2013-07-09 15:45:28 +0200

    fix intendation

--- lib/cmd.ex
+++ lib/cmd.ex
@@ -1,33 +1,33 @@
 defmodule Cmd do
-	def all <<command::binary>> do
-		all Node.self, command
-	end
+  def all <<command::binary>> do
+    all Node.self, command
+  end
 
-	def all sender, <<command::binary>> do
-		nodes = [Node.self | Node.list]
-		execute sender, nodes, command
-	end
+  def all sender, <<command::binary>> do
+    nodes = [Node.self | Node.list]
+    execute sender, nodes, command
+  end
 
-	defp execute sender, list, <<command::binary>> do
-		if length(list) > 0 do
-			[head|rest] = list
+  defp execute sender, list, <<command::binary>> do
+    if length(list) > 0 do
+      [head|rest] = list
 
-			execute sender, rest, command
+      execute sender, rest, command
 
-			Node.spawn(head, Cmd, :local, [sender, command])
-		end
-	end
+      Node.spawn(head, Cmd, :local, [sender, command])
+    end
+  end
 
-	def local <<command::binary>> do
-		System.cmd command
-	end
+  def local <<command::binary>> do
+    System.cmd command
+  end
 
-	def local sender, <<command::binary>> do
-		result = System.cmd command
+  def local sender, <<command::binary>> do
+    result = System.cmd command
 
-		if sender do
-			text = "#{Node.self} says: #{result}"
-			Node.spawn(sender, IO, :puts, [text])
-		end
-	end
+    if sender do
+      text = "#{Node.self} says: #{result}"
+      Node.spawn(sender, IO, :puts, [text])
+    end
+  end
 end
