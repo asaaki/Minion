32a5f18e399aa653da3f2fa4e79c5500f4f6441b
Steffen Schröder <steffen@schroeder-blog.de>
2013-07-29 17:05:54 +0200

    more docs

--- lib/cmd.ex
+++ lib/cmd.ex
@@ -1,6 +1,10 @@
 defmodule Cmd do
   @moduledoc """
-  This module provides allow you to execute remote shell commands and get back the output.
+  This module allows you to execute remote shell commands and get back the output.
+  """
+
+  @doc """
+  Runs a shell command on all nodes including yourself
 
   ## Examples
 
@@ -9,32 +13,35 @@ defmodule Cmd do
       #=> minion@MacBook-Air.local says: Darwin Kernel Version 12.4.0: Wed May  1 17:57:12 PDT 2013; root:xnu-2050.24.15~1/RELEASE_X86_64
       #=> minion@raspberry.local says: #1 PREEMPT Sun Jul 21 17:39:58 CDT 2013
   """
-
-  @doc "Executes command on all nodes including yourself"
-  def all <<command::binary>> do
+  def all command do
     all Node.self, command
   end
 
-  def all sender, <<command::binary>> do
+  @doc """
+  Runs a shell command on all nodes including yourself and specifies a sender that gets back the output
+  """
+  def all sender, command do
     nodes = [Node.self | Node.list]
     execute sender, nodes, command
   end
 
   @doc "Executes command on all nodes except yourself"
-  def other <<command::binary>> do
+  def other command do
     other Node.self, command
   end
 
-  def other sender, <<command::binary>> do
+  @doc "Executes command on all nodes except yourself and specifies a sender that gets back the output"
+  def other sender, command do
     execute sender, Node.list, command
   end
 
   @doc "Executes command on all nodes except the given nodes"
-  def except nodes, <<command::binary>> do
+  def except nodes, command do
     except Node.self, nodes, command
   end
 
-  def except sender, nodes, <<command::binary>> do
+  @doc "Executes command on all nodes except the given nodes and specifies a sender that gets back the output"
+  def except sender, nodes, command do
     all_nodes = [Node.self | Node.list]
 
     Enum.each(all_nodes, fn(node) ->
@@ -45,15 +52,16 @@ defmodule Cmd do
   end
 
   @doc "Executes command only on the given nodes"
-  def only nodes, <<command::binary>> do
+  def only nodes, command do
     only Node.self, nodes, command
   end
 
-  def only sender, nodes, <<command::binary>> do
+  @doc "Executes command only on the given nodes and specifies a sender that gets back the output"
+  def only sender, nodes, command do
     execute sender, nodes, command
   end
 
-  def execute sender, list, <<command::binary>> do
+  defp execute sender, list, command do
     if length(list) > 0 do
       [head|rest] = list
 
@@ -61,18 +69,35 @@ defmodule Cmd do
 
       Node.spawn(head, Cmd, :local, [sender, command])
     end
+
+    :ok
   end
 
-  def local <<command::binary>> do
+  @doc """
+  Runs a shell command on your current node and return output as String
+
+  ## Examples
+
+      Cmd.local "uname -v"  
+      # => "Darwin Kernel Version 12.4.0: Wed May  1 17:57:12 PDT 2013; root:xnu-2050.24.15~1/RELEASE_X86_64\\n"
+  """
+  def local command do
+    <<command::binary>> = command
     System.cmd command
   end
 
-  def local sender, <<command::binary>> do
+  @doc """
+  Runs a shell command on your current node and return output on then senders conlose
+  """
+  def local sender, command do
+    <<command::binary>> = command
     result = System.cmd command
 
     if sender do
       text = "#{Node.self} says: #{result}"
       Node.spawn(sender, IO, :puts, [text])
     end
+
+    :ok
   end
 end
