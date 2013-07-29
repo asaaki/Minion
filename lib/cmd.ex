fa6c69564d000127171c7904105a2d0e24bbbe3f
Steffen Schröder <steffen@schroeder-blog.de>
2013-07-29 11:22:11 +0200

    docs, kind of ;)

--- lib/cmd.ex
+++ lib/cmd.ex
@@ -1,4 +1,16 @@
 defmodule Cmd do
+  @moduledoc """
+  This module provides allow you to execute remote shell commands and get back the output.
+
+  ## Examples
+
+      Cmd.all "uname -v"
+      #=> #PID<0.101.0>
+      #=> minion@MacBook-Air.local says: Darwin Kernel Version 12.4.0: Wed May  1 17:57:12 PDT 2013; root:xnu-2050.24.15~1/RELEASE_X86_64
+      #=> minion@raspberry.local says: #1 PREEMPT Sun Jul 21 17:39:58 CDT 2013
+  """
+
+  @doc "Executes command on all nodes including yourself"
   def all <<command::binary>> do
     all Node.self, command
   end
@@ -8,6 +20,7 @@ defmodule Cmd do
     execute sender, nodes, command
   end
 
+  @doc "Executes command on all nodes except yourself"
   def other <<command::binary>> do
     other Node.self, command
   end
@@ -16,6 +29,7 @@ defmodule Cmd do
     execute sender, Node.list, command
   end
 
+  @doc "Executes command on all nodes except the given nodes"
   def except nodes, <<command::binary>> do
     except Node.self, nodes, command
   end
@@ -30,6 +44,7 @@ defmodule Cmd do
     end)
   end
 
+  @doc "Executes command only on the given nodes"
   def only nodes, <<command::binary>> do
     only Node.self, nodes, command
   end
--- mix.exs
+++ mix.exs
@@ -4,6 +4,9 @@ defmodule Minion.Mixfile do
   def project do
     [ app: :minion,
       version: "0.0.1",
+      name: "Minion",
+      source_url: "https://github.com/ChaosSteffen/Minion",
+      homepage_url: "https://github.com/ChaosSteffen/Minion",
       deps: deps ]
   end
 
@@ -16,6 +19,6 @@ defmodule Minion.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [ { :ex_doc, github: "elixir-lang/ex_doc" } ]
   end
 end
